9385e71e16fadbebdf42de879f1b740e4a8c38c5
Ahsan Habib Niloy <habibniloy500@gmail.com>
2025-01-13 10:19:49 -0800

    - some incommited changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,41 @@
-const { watch } = require("fs");
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // Import HtmlWebpackPlugin
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
+  mode: "production", // Set to production for final build
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    clean: true, // Clean the dist folder before building
   },
-  watch: true,
   module: {
     rules: [
       {
-        test: /\.css$/, // Handle CSS files
+        test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg|jpeg|gif|svg)$/i, // Handle image files
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|eot|ttf|otf)$/i, // Handle images and fonts
         type: "asset/resource",
         generator: {
-          filename: "assets/[name][ext]", // Store images in 'assets' folder
+          filename: "assets/[name][ext]", // Store assets in 'assets' folder
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html", // Specify the HTML template
+      template: "./index.html",
+      filename: "index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "asset", to: "asset" }, // Copy assets folder
+        { from: "style", to: "style" }, // Copy style folder
+        { from: "fonts", to: "fonts" }, // Copy fonts folder
+      ],
     }),
   ],
 };
